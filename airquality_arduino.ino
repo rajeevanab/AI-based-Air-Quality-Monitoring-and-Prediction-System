1c80f59cc8b9d615ef95df1181f6315916464580
rajeevanab <79514672+rajeevanab@users.noreply.github.com>
2021-03-02 11:50:01 +0530

    updated
    arduino updated

--- airquality_arduino.ino
+++ airquality_arduino.ino
@@ -1,10 +1,10 @@
 void setup() {
 
-Serial.begin(9600);
+Serial.begin(9600); // setting the board rate
 }
 
 void loop() {
  
-int sen= analogRead(A0);
-print(sen);
+int sen= analogRead(A0); // reading the analog values from the sensor 
+print(sen); //print the sensor values
 }
